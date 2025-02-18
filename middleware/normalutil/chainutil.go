b8b76bb44329b37d8f920170afd36eef2a1c1bb1
lzx900724@sina.com <leizx@glodon.com>
2025-02-18 17:40:11 +0800

    新的责任链处理器

--- middleware/normalutil/chainutil.go
+++ middleware/normalutil/chainutil.go
@@ -1,23 +1,56 @@
 package normalutil
 
-type ChainResultWrapper[R interface{}] struct {
-	Result  R
-	EndFlag bool
+type MatchFunc[Handler interface{}] func(handler Handler, inputParams []any, lastResults []any) bool
+
+type ExecuteFunc[Handler interface{}] func(handler Handler, inputParams []any, lastResults []any) ([]any, bool)
+
+type ChainExecuteRequest[Handler interface{}] struct {
+	executors   []Handler
+	matchFunc   MatchFunc[Handler]
+	execFunc    ExecuteFunc[Handler]
+	inputParams []any
+	results     []any
 }
 
-func ExecuteChain[T interface{}, R interface{}](executors []T, matcher func(T, R) bool, execFunc func(T, R) *ChainResultWrapper[R]) R {
-	var result R
-	for _, executor := range executors {
-		if matcher(executor, result) {
-			wrapper := execFunc(executor, result)
-			if wrapper == nil {
-				break
-			}
-			result = wrapper.Result
-			if wrapper.EndFlag {
+func NewChainExecuteRequest[Handler interface{}](executors []Handler, matchFunc MatchFunc[Handler],
+	execFunc ExecuteFunc[Handler], inputParams ...any) *ChainExecuteRequest[Handler] {
+	return &ChainExecuteRequest[Handler]{
+		executors:   executors,
+		matchFunc:   matchFunc,
+		execFunc:    execFunc,
+		inputParams: inputParams,
+	}
+}
+
+func (c *ChainExecuteRequest[Handler]) Do() {
+	var results []any
+	for _, executor := range c.executors {
+		if c.matchFunc(executor, c.inputParams, results) {
+			curResults, isEnd := c.execFunc(executor, c.inputParams, results)
+			results = curResults
+			if isEnd {
 				break
 			}
 		}
 	}
-	return result
+	if results == nil {
+		results = make([]any, 0)
+	}
+	c.results = results
+}
+
+func GetResult[Handler interface{}, RetType interface{}](c *ChainExecuteRequest[Handler],
+	index int,
+	defVal RetType) (RetType, bool) {
+	if len(c.results) == 0 || len(c.results) <= index {
+		return defVal, false
+	}
+	resVal := c.results[index]
+	var retVal RetType
+	var ok bool
+	retVal, ok = resVal.(RetType)
+	if !ok {
+		return defVal, false
+	}
+	return retVal, true
 }
